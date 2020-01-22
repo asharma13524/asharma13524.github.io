b2f11d4d748c099177868bc76562fbc0756dc1eb
abhin <asharma13524@gmail.com>
2020-01-21 23:54:15 -0600

    added video logic

---
+++ player.js
@@ -0,0 +1,32 @@
+const myVideo = document.getElementById('myVideo');
+const btnPlay = document.getElementById('btnPlay');
+const btnPause = document.getElementById('btnPause');
+const btnStop = document.getElementById('btnStop');
+
+btnPlay.addEventListener('click', vidAction);
+btnPause.addEventListener('click', vidAction);
+btnStop.addEventListener('click', vidAction);
+myVideo.addEventListener('ended', vidAction);
+
+
+function vidAction(event) {
+    switch (event.target.id) {
+        case "btnPlay":
+            playVideo();
+            timer = setInterval(update, 100);
+            break;
+        case "btnPause":
+            myVideo.pause();
+            break;
+        case "btnStop":
+            myVideo.pause();
+            myVideo.currentTime = 0;
+            break;
+    }
+}
+
+function playVideo() {
+    myVideo.play();
+    myVideo2.play();
+    timer = setInterval(update, 100);
+}
